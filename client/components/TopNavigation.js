fa128b509a4d133504f86bd70bf3459ead659265
mahdizaabi <mahdi.zaabi@gmail.com>
2021-08-05 17:49:58 +0200

    add local state for active links

--- client/components/TopNavigation.js
+++ client/components/TopNavigation.js
@@ -1,25 +1,31 @@
 
-import {Menu} from "antd";
+import { Menu } from "antd";
 import Link from "next/link";
-import {AppstoreOutlined, LoginOutlined, UserAddOutlined} from '@ant-design/icons'
+import { AppstoreOutlined, LoginOutlined, UserAddOutlined } from '@ant-design/icons'
+import { useEffect, useState } from "react";
 
-const {Item} = Menu;
+const { Item } = Menu;
 const TopNavigation = () => {
+    const [currentPath, setCurrentPath] = useState('');
+    useEffect(() => {
+        process.browser && setCurrentPath(window.location.pathname)
+
+    }, [process.browser && window.location.pathname])
     return (
-        <Menu mode="horizontal">
-            <Item icon={<AppstoreOutlined />}>
+        <Menu mode="horizontal" selectedKeys={[currentPath]}>
+            <Item key="/" onClick={(e) => setCurrentPath(e.key)} icon={<AppstoreOutlined />}>
                 <Link href="/">
                     <a className={"type"}>App</a>
                 </Link>
             </Item>
 
-            <Item icon={<LoginOutlined />}> 
+            <Item key="/login" onClick={(e) => setCurrentPath(e.key)} icon={<LoginOutlined />}>
                 <Link href="/login">
                     <a className={"type"}>Login</a>
                 </Link>
             </Item>
 
-            <Item icon={<UserAddOutlined/>}>
+            <Item key="/regiser " onClick={(e) => setCurrentPath(e.key)} icon={<UserAddOutlined />}>
                 <Link href="/register">
                     <a>Register</a>
                 </Link>
