c46692d1aaee79660701b54f320ba1be4dbaa834
mahdizaabi <mahdi.zaabi@gmail.com>
2021-08-07 00:45:31 +0200

    add csrf protection layer

--- server/server.js
+++ server/server.js
@@ -2,7 +2,13 @@ import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
 const morgan = require('morgan');
+import csrf from "csurf";
+
+import cookieParser from 'cookie-parser'
+
+
 const authRoute = require('./routes/auth');
+const csrfProtection = csrf({ cookie: true })
 
 require('dotenv').config();
 
@@ -11,27 +17,32 @@ const app = express();
 
 
 //database connexion
-mongoose.connect(process.env.DATABASE,{
-    useNewUrlParser:true,
-    useFindAndModify:false,
-    useUnifiedTopology:true,
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
     useCreateIndex: true
-}).then(()=>console.log('db cnnected')).catch(e=>console.log(e))
- 
+}).then(() => console.log('db cnnected')).catch(e => console.log(e))
+
 app.use(cors());
 app.use(express.json());
+
+app.use(cookieParser())
 app.use(morgan("dev"));
 app.use((req, res, next) => {
     console.log("middleware is executed")
     next()
 })
-
-
 //routes:
-
-
 app.use('/api', authRoute);
 
+//csrf
+app.use(csrfProtection)
+
+
+app.get('/api/csrf-token', (req, res) => {
+    res.json({ csrfToken: req.csrfToken() })
+})
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
