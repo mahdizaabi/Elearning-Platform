ccb67946c0893b209130ab4a3521902941d9665e
mahdizaabi <mahdi.zaabi@gmail.com>
2021-08-22 12:38:34 +0200

    implement the course create view

--- client/pages/instructor/course/create.js
+++ client/pages/instructor/course/create.js
@@ -1,10 +1,15 @@
 import InstructorRoute from '../../../components/routes/InstructorRoute'
 import CourseCreationFrom from '../../../components/forms/courseCreationForm'
 import { useState } from 'react'
+import Resizer from 'react-image-file-resizer';
+import { toast } from 'react-toastify';
+import axios from 'axios';
 
 const CourseCreate = () => {
 
     const [preview, setPreview] = useState("");
+    const [uploadButtonText, setUploadButtonText] = useState("");
+    const [image, setImage] = useState("");
     const [course, setCourse] = useState({
         name: '',
         description: '',
@@ -15,14 +20,33 @@ const CourseCreate = () => {
         loading: false,
         imagePreview: ''
     })
-
+ 
     const handleChange = e => {
         setCourse({ ...course, [e.target.name]: e.target.value })
     }
 
+
     const handleImage = (e) => {
-        console.log(window.URL.createObjectURL(e.target.files[0]));
-        setPreview(window.URL.createObjectURL(e.target.files[0]));
+        let file = e.target.files[0]
+        setPreview(window.URL.createObjectURL(file));
+        setUploadButtonText(file.name);
+        setCourse({ ...course, loading: true })
+        Resizer.imageFileResizer(file, 700, 500, "JPEG", 100, 0, async (imageUri) => {
+            try {
+                setCourse({ ...course, loading: true })
+                let { data } = await axios.post('/api/course/upload-image', {
+                    image: imageUri
+                })
+                console.log(data)
+                setCourse({ ...course, loading: false })
+                setImage(data.imageUri)
+                
+            } catch (error) {
+                console.log(error)
+                setCourse({ ...course, loading: false })
+                toast("Image upload failed, try later")
+            }
+        })
 
     }
     const handleSubmit = (e) => {
@@ -39,7 +63,9 @@ const CourseCreate = () => {
                 setCourse={setCourse}
                 preview={preview}
                 setPreview={setPreview}
+                uploadButtonText={uploadButtonText}
             ></CourseCreationFrom>
+          {image &&  <img src={`https://basicstorage1414.blob.core.windows.net/epimages/${image}`} alt="dd"/>} 
         </InstructorRoute>
     </>)
 
