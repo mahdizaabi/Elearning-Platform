5b2f1103cec326621d08b741ec757a70e79983d6
mahdizaabi <mahdi.zaabi@gmail.com>
2021-08-22 12:37:25 +0200

    limit the maximal size of post request

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ mongoose.connect(process.env.DATABASE, {
     useCreateIndex: true
 }).then(() => console.log('db cnnected')).catch(e => console.log(e))
 app.use(cors());
-app.use(express.json());
+app.use(express.json({limit: "5mb"}));
 app.use(cookieParser())
 app.use(morgan("dev"));
 
